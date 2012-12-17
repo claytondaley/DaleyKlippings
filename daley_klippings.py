88229bed9dbf81c7a908eae300f47a2a2e8a9199
Clayton Daley <clayton.daley@gmail.com>
2012-12-16 21:15:13 -0500

    - update version number

--- daley_klippings.py
+++ daley_klippings.py
@@ -5,7 +5,7 @@
 Main DaleyKlippings window
 """
 
-__ver__ = '0.1.0'
+__ver__ = '0.2'
 ## Features: 
 ## - default import & export patterns
 ## - language settings for highlight, note & bookmark terms
