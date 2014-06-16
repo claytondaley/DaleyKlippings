e881f0fa5fb88a8572cbf6147c3c58d45b2f9962
Clayton Daley <clayton.daley@gmail.com>
2014-06-16 15:45:53 -0400

    - Moved UI helpers (QStyledItemDelegate) for MainWin into same file as MainWin

--- daley_klippings.py
+++ daley_klippings.py
@@ -31,10 +31,106 @@ logger = logging.getLogger("daley_klippings")
 logger.addHandler(handler)
 logger.info("Loading DaleyKlippings")
 
+from table import *  # implicitly imports settings
 from gui.ui_mainWin import *
-from table import *
-from settings import *
 import unicodecsv as csv
+
+
+# Helper classes for table UI in main window
+class DateEditDelegate(QStyledItemDelegate):
+    """
+    Date edit delegate, attempts to localize dates, defaulting to 'dd.MM.yy, hh:mm'
+    ('%d.%m.%y, %H:%M' in Python notation)
+    """
+
+    def __init__(self, parent=None, format=None):
+        QStyledItemDelegate.__init__(self, parent)
+        if format is not None:
+            self.format = format
+        else:
+            self.format = 'dd.MM.yy, hh:mm'
+
+    def createEditor(self, parent, option, index):
+        editor = QDateTimeEdit(parent)
+        editor.setCalendarPopup(True)
+        editor.setDisplayFormat(self.format)
+        return editor
+
+    def setEditorData(self, editor, index):
+        editor.setDateTime(QDateTime(index.model().data(index, Qt.EditRole)))
+
+    def setModelData(self, editor, model, index):
+        model.setData(index, editor.dateTime().toPython(), Qt.EditRole)
+
+    def updateEditorGeometry(self, editor, option, index):
+        editor.setGeometry(option.rect)
+
+    def displayText(self, value, locale):
+        if u'%' in self.format:
+            # The % sign indicates that the pattern is a basic Python format
+            return value.strftime(self.format)
+        else:
+            # Otherwise, we assume a Qt Format
+            return QDateTime(value).toString(self.format)
+
+
+class ComboBoxDelegate(QStyledItemDelegate):
+    """
+    Type of the note edit delegate, QComboBox with 3 predefined values:
+    Highlight, Bookmark, Note.
+    """
+
+    def __init__(self, parent=None):
+        QStyledItemDelegate.__init__(self, parent)
+
+    def createEditor(self, parent, option, index):
+        editor = QComboBox(parent)
+        # editor.setAutoCompletion(True)
+        # Load language settings
+        settings = Settings()['Application Settings']['Language']
+        highlight = (settings['Highlight'], 'Highlight')[settings['Highlight'] == '']
+        note = (settings['Note'], 'Note')[settings['Note'] == '']
+        bookmark = (settings['Bookmark'], 'Bookmark')[settings['Bookmark'] == '']
+
+        editor.addItems([highlight, note, bookmark])
+        return editor
+
+    def setEditorData(self, editor, index):
+        value = index.model().data(index, Qt.EditRole)
+        editor.setCurrentIndex(editor.findText(value))
+
+    def setModelData(self, editor, model, index):
+        value = editor.itemText(editor.currentIndex())
+        model.setData(index, value, Qt.EditRole)
+
+    def updateEditorGeometry(self, editor, option, index):
+        editor.setGeometry(option.rect)
+
+
+class LocationEditDelegate(QStyledItemDelegate):
+    """
+    Loctaion edit delegate, QLineEdit with mask
+    """
+
+    def __init__(self, parent=None):
+        QStyledItemDelegate.__init__(self, parent)
+
+    def createEditor(self, parent, option, index):
+        editor = QLineEdit(parent)
+        #validator = QRegExpValidator(QRegExp('([0-9]+-?)*'), editor)
+        #editor.setValidator(validator)
+        return editor
+
+    def setEditorData(self, editor, index):
+        value = index.model().data(index, Qt.EditRole)
+        editor.setText(value)
+
+    def setModelData(self, editor, model, index):
+        value = editor.text()
+        model.setData(index, value, Qt.EditRole)
+
+    def updateEditorGeometry(self, editor, option, index):
+        editor.setGeometry(option.rect)
 
 
 class MainWin(QMainWindow):
@@ -117,7 +213,7 @@ class MainWin(QMainWindow):
         # Try to localize the dates
         date_language = self.settings['Application Settings']['Language']['Date Language']
         if date_language == "English (default)":
-            # This is a default value not available in QLocale
+            # This is our default value, and needs to be translated into a valid language in QLocale
             date_language = "English"
         logger.info("Date Language is %s" % date_language)
         local_language = QLocale(getattr(QLocale, date_language))
--- table.py
+++ table.py
@@ -25,105 +25,9 @@ logger.info("Loading DaleyKlippings Table Models")
 """
 Table data model, proxy model, data edit delegates, parsing routine, default constants
 """
-from settings import *  # Includes re, PySide
+from settings import *  # Includes re, PySide, pformat
 import itertools
 from datetime import datetime as dt
-
-
-class DateEditDelegate(QStyledItemDelegate):
-    """
-    Date edit delegate, set date format to 'dd.MM.yy, hh:mm' or 
-    '%d.%m.%y, %H:%M' in Python designations
-    """
-
-    def __init__(self, parent=None, format=None):
-        QStyledItemDelegate.__init__(self, parent)
-        if format is not None:
-            self.format = format
-        else:
-            self.format = 'dd.MM.yy, hh:mm'
-
-    def createEditor(self, parent, option, index):
-        editor = QDateTimeEdit(parent)
-        editor.setCalendarPopup(True)
-        editor.setDisplayFormat(self.format)
-        return editor
-
-    def setEditorData(self, editor, index):
-        editor.setDateTime(QDateTime(index.model().data(index, Qt.EditRole)))
-
-    def setModelData(self, editor, model, index):
-        model.setData(index, editor.dateTime().toPython(), Qt.EditRole)
-
-    def updateEditorGeometry(self, editor, option, index):
-        editor.setGeometry(option.rect)
-
-    def displayText(self, value, locale):
-        if u'%' in self.format:
-            # The % sign indicates that the pattern is a basic Python format
-            return value.strftime(self.format)
-        else:
-            # Otherwise, we assume a Qt Format
-            return QDateTime(value).toString(self.format)
-
-
-class ComboBoxDelegate(QStyledItemDelegate):
-    """
-    Type of the note edit delegate, QComboBox with 3 predefined values:
-    Highlight, Bookmark, Note.
-    """
-
-    def __init__(self, parent=None):
-        QStyledItemDelegate.__init__(self, parent)
-
-    def createEditor(self, parent, option, index):
-        editor = QComboBox(parent)
-        # editor.setAutoCompletion(True)
-        # Load language settings
-        settings = Settings()['Application Settings']['Language']
-        highlight = (settings['Highlight'], 'Highlight')[settings['Highlight'] == '']
-        note = (settings['Note'], 'Note')[settings['Note'] == '']
-        bookmark = (settings['Bookmark'], 'Bookmark')[settings['Bookmark'] == '']
-
-        editor.addItems([highlight, note, bookmark])
-        return editor
-
-    def setEditorData(self, editor, index):
-        value = index.model().data(index, Qt.EditRole)
-        editor.setCurrentIndex(editor.findText(value))
-
-    def setModelData(self, editor, model, index):
-        value = editor.itemText(editor.currentIndex())
-        model.setData(index, value, Qt.EditRole)
-
-    def updateEditorGeometry(self, editor, option, index):
-        editor.setGeometry(option.rect)
-
-
-class LocationEditDelegate(QStyledItemDelegate):
-    """
-    Loctaion edit delegate, QLineEdit with mask
-    """
-
-    def __init__(self, parent=None):
-        QStyledItemDelegate.__init__(self, parent)
-
-    def createEditor(self, parent, option, index):
-        editor = QLineEdit(parent)
-        #validator = QRegExpValidator(QRegExp('([0-9]+-?)*'), editor)
-        #editor.setValidator(validator)
-        return editor
-
-    def setEditorData(self, editor, index):
-        value = index.model().data(index, Qt.EditRole)
-        editor.setText(value)
-
-    def setModelData(self, editor, model, index):
-        value = editor.text()
-        model.setData(index, value, Qt.EditRole)
-
-    def updateEditorGeometry(self, editor, option, index):
-        editor.setGeometry(option.rect)
 
 
 # Changed structure to list from dictionary to avoid problems with implementation of
@@ -214,7 +118,6 @@ class Clippings(list):
         for raw_clipping in raw_clippings:
             clipNo += 1
             try:
-                #if c.strip() == '' : continue
                 clipping = pattern.search(raw_clipping.strip())
                 line = {}
                 emptyHeaders = 0
